bbe7f539eed6bead536db46e05685e30799553a0
haroldwhite382-hub <haroldwhite382@gmail.com>
2026-06-26 10:08:02 +0530

    update

--- JavaScript /Script.js
+++ JavaScript /Script.js
@@ -1,14 +1,11 @@
 window.onload = function() {
-  let target = document.getElementById('jarvis-text');
-  let message = "Welcome to the Stark Exhibit, Agent 2010328.";
-  let index = 0;
-
-  let interval = setInterval(function() {
-    target.innerHTML += message[index];
-    index++;
-
-    if (index === message.length) {
-      clearInterval(interval);
-    }
-  }, 80);
-};
+    let target = document.getElementById('jarvis-text');
+    let message = "Welcome to the Stark Exhibit,Agent 2010328";
+    let index = 0;
+    let interval = setInterval(function() {target.innerHTML += message[index];
+        index++;
+        if (index === message.length) {clearInterval(interval);
+            
+        }
+    }, 80);
+}
