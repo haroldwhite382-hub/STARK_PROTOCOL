25a25655136cc1f06ca6f9940c6b6746c89eb32a
haroldwhite382-hub <haroldwhite382@gmail.com>
2026-06-26 19:53:20 +0530

    update

--- JavaScript /Halls/hall_1.js
+++ JavaScript /Halls/hall_1.js
@@ -1,8 +1,7 @@
-window.onload = function() {
-  document.getElementById('next-btn').onclick = function() {
-    document.body.style.opacity = '0';
-    setTimeout(function() {
-      window.location.href = '/Pages/hall_2.html';
-    }, 500);
-  };
+
+document.getElementById('next-btn').onclick = function() {
+  document.body.style.opacity = '0';
+  setTimeout(function() {
+    window.location.href = '../Pages/hall_2.html';
+  }, 500);
 };
