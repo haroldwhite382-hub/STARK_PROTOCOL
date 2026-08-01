65e660918a7717dbcd011e6407c7dc862fc59423
haroldwhite382-hub <haroldwhite382@gmail.com>
2026-08-01 16:23:42 +0530

    update

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
-document.getElementById("e").onclick = function() {
-  document.getElementById("ironman").style.animation = "exitIronman 1s forwards";
-  document.getElementById("title").style.animation = "fadeOut 0.5s forwards";
+document.getElementById("e").onclick = function () {
 
-  setTimeout(function() {
-    window.location.href = "../Pages/hall_1.html";
-  }, 1000);
+    document.getElementById("ironman-container").style.animation =
+        "exitIronman 1s forwards";
+    document.getElementById("intro").style.animation =
+        "fadeOut 0.5s forwards";
+    document.getElementById("e").style.animation =
+        "fadeOut 0.5s forwards";
+    setTimeout(function () {
+        window.location.href = "../Pages/hall_1.html";
+    }, 1000);
+
 };
